554b9ac3cbf00bbf2a7dec0e20afbe9579e91bfe
Mohammed Rangwala <rangwalamhmd@gmail.com>
2026-04-19 00:02:39 +0530

    cors policy added

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import add_contact, toggle_driving
+from .views import add_contact, toggle_driving, contact_list
 
 urlpatterns = [
     path('add-contact/', add_contact, name='add_contact'),
     path('toggle-driving/', toggle_driving, name='toggle_driving'),
-    path('contact_list/', contact_list)
+    path('contact_list/', contact_list, name='contact_list')
 ]
