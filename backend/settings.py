67af323c89599838fb41a578b7f0d200831d8b5b
Mohammed Rangwala <rangwalamhmd@gmail.com>
2026-04-22 18:15:12 +0530

    API's Added

--- backend/settings.py
+++ backend/settings.py
@@ -58,8 +58,8 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 ALLOWED_HOSTS = [
     "smart-driving-auto-reply-system.onrender.com",
-    "localhost",
-    "127.0.0.1"
+    "127.0.0.1",
+    "localhost"
 ]
 
 TEMPLATES = [
